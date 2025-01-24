1cf4b8932c9adbf9412a82684c740afe6f668154
Tuong Nguyen <yolotube21@yahoo.com>
2025-01-24 15:47:56 -0500

    Task 2 - Accessing & Modifying Arrays

--- cc_2.js
+++ cc_2.js
@@ -1,7 +1,18 @@
 let fishTypes = ["Salmon", "Tuna", "Mackerel", "Bass", "Sardine", "Herring"];
 fishTypes.push("Cod");
-console.log(fishTypes); // Outcome that includes "Cod"
+console.log(fishTypes); // Output: Original array that includes "Cod"
 
 fishTypes.pop();// Removes "Cod"
 
-console.log(fishTypes);// Outcome removes "Cod" and updates array
+console.log(fishTypes);// Output: Original array that removes "Cod"
+
+// Task 2 - Accessing & Modifying Arrays
+let scores = [85, 95, 78, 92, 88, 55];
+
+scores[1] = 78; // Updates score in 1
+console.log(scores[1]); // outputs new score in 1
+
+let totalScore = scores.reduce((sum, scores) => sum + scores, 0); //Sums scores
+console.log(totalScore); //Output of scores
+let average = totalScore / scores.length;
+console.log("Average:", average);
