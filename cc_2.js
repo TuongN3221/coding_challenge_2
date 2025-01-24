92cfb1c9ded0fc727c6bb8aa433547f7f51cfa50
Tuong Nguyen <yolotube21@yahoo.com>
2025-01-24 14:42:42 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+let fishTypes = ["Salmon", "Tuna", "Mackerel", "Bass", "Sardine", "Herring"];
+fishTypes.push("Cod");
+console.log(fishTypes); // Outcome that includes "Cod"
+
+fishTypes.pop();// Removes "Cod"
+
+console.log(fishTypes);// Outcome removes "Cod" and updates array
